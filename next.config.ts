142d35d492664899fbe9f30bc8a673e7b2513715
David Aleksanian <davidaleksanian@MacBookAir.fibertel.com.ar>
2025-06-29 21:11:54 -0300

    Fix: Allow external Blob domain for images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['klfmasfbi2f0kmeu.public.blob.vercel-storage.com'], // укажи свой домен точно
+  },
 };
 
 export default nextConfig;
-
